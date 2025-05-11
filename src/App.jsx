cf3e3ed00c6653ab6c30042267d4b0233b0b7515
Leila237 <mbiydzenyuyeileen@gmail.com>
2025-05-11 17:12:57 +0100

    created branch for frontend

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ export default function App() {
       <main className="container">
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </main>
       <Footer />
