8dee43ce430f5ff3d2fac2aaf3d66f2c4adb873a
Leila237 <mbiydzenyuyeileen@gmail.com>
2025-05-30 17:20:31 +0100

    added rate limiting to all app request

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -2,9 +2,8 @@ import {
   findByEmail,
   createUserRecord,
   comparePassword,
-} from "../models/user-model.js";
-import jwt from "jsonwebtoken";
-
+} from '../models/user-model.js'
+import jwt from 'jsonwebtoken'
 
 // Generate JWT Token
 const generateToken = (user) => {
@@ -12,51 +11,51 @@ const generateToken = (user) => {
     { id: user.id, email: user.email, username: user.username },
     process.env.JWT_SECRET,
     { expiresIn: process.env.JWT_EXPIRES_IN }
-  );
-};
+  )
+}
 
 // Register User
 export async function registerUser({ username, email, password }) {
   try {
-    const existing = await findByEmail(email);
+    const existing = await findByEmail(email)
     if (existing) {
-      throw new Error("Email already in use.");
+      throw new Error('Email already in use.')
     }
 
-    const user = await createUserRecord({ username, email, password });
+    const user = await createUserRecord({ username, email, password })
 
     return {
       success: true,
-      message: "User registered successfully.",
+      message: 'User registered successfully.',
       data: user,
-    };
+    }
   } catch (err) {
-    throw new Error(`Registration failed: ${err.message}`);
+    throw new Error(`Registration failed: ${err.message}`)
   }
 }
 
 // Login User
 export async function loginUser({ email, password }) {
   try {
-    const user = await findByEmail(email);
+    const user = await findByEmail(email)
     if (!user) {
-      throw new Error("User not found.");
+      throw new Error('User not found.')
     }
 
-    const isMatch = await comparePassword(password, user.password);
+    const isMatch = await comparePassword(password, user.password)
     if (!isMatch) {
-      throw new Error("Invalid email or password.");
+      throw new Error('Invalid email or password.')
     }
 
-    const token = generateToken(user);
+    const token = generateToken(user)
 
     return {
       success: true,
-      message: "User logged in successfully.",
+      message: 'User logged in successfully.',
       token,
       data: { id: user.id, email: user.email, username: user.username },
-    };
+    }
   } catch (err) {
-    throw new Error(`Login failed: ${err.message}`);
+    throw new Error(`Login failed: ${err.message}`)
   }
 }
